f2dcce8f6e8dbcdfe9b3473af961d350e5f98ade
31antorkhan2006 <31antorkhan2006@gmail.com>
2026-07-19 23:31:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("AYZO Website Loaded Successfully!");
-});
+});const images=[
+"https://picsum.photos/1200/400?random=1",
+"https://picsum.photos/1200/400?random=2",
+"https://picsum.photos/1200/400?random=3"
+];
+
+let i=0;
+setInterval(()=>{
+  i=(i+1)%images.length;
+  document.getElementById("slide").src=images[i];
+},3000);
